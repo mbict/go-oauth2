438c0681b1ef49510de6e196e1bbc204a98c465c
Michael Boke <michael.boke@gmail.com>
2018-01-21 21:49:08 +0100

    added scope starategy

--- scope_strategy.go
+++ scope_strategy.go
@@ -21,6 +21,35 @@ func DefaultScopeStrategy(scopes Scope, needle ...string) bool {
 	return true
 }
 
+// HirarchicalScopeStrategy does a partial match  foo == foo.bar
 func HirarchicalScopeStrategy(scopes Scope, needle ...string) bool {
-	return false
+	comp := func(needle string) bool {
+		for _, scope := range scopes {
+			scopeLen := len(scope)
+			needleLen := len(needle)
+
+			//exact match
+			// true : abc.def == abc.def
+			// false: abc.def == foo.bar
+			// false: xyz == foo.bar
+			if (needleLen == scopeLen && scope == needle) {
+				return true
+			}
+
+			//partial match match
+			// true : abc == abc.def
+			// false: abc.def == abc
+			if (needleLen > scopeLen && scope == needle[:scopeLen] && needle[scopeLen] == '.') {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, want := range needle {
+		if want != "" && false == comp(want) {
+			return false
+		}
+	}
+	return true
 }
